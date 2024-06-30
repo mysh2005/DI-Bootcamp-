ed1310aad2dd6198e479e9b0c304002d1d389839
mysh2005 <myshti2005@icloud.com>
2024-06-30 15:49:34 +0400

    Add files via upload

---
+++ Week 2/Day 2/Exercice XP/Exercise5.py
@@ -0,0 +1,14 @@
+size = input("Enter the size T-Shirt: ")
+
+text = input("Enter the text of message to be printed: ")
+
+
+def make_shirt(size, text):
+
+	print(f"The size of the shirt is {size} and the text is {text}")
+
+make_shirt("Large", "I love Python")
+
+make_shirt("Medium", "I love Python")
+
+make_shirt(size, text)
